ff3ac0eb95410be2c454aedcf8c79ff62758eaf1
Andrew Carnesi <andrew.carnesi@outlook.com>
2026-06-02 10:41:52 -0500

    -Added progress bar to Level Progression for gurud kanji to level -Styling updates to Subject Badges -Switched vocab in level progression to be a toggleable view #3

--- src/components/LevelProgression.vue
+++ src/components/LevelProgression.vue
@@ -1,26 +1,44 @@
 <template>
     <div class="m-6 p-6 border-purple-500 border-2 rounded-xl h-fit">
-        <div class="mb-6"><span class="text-2xl">Level Progression</span></div>
-        <div class="mb-3"><span class="text-xl">Radicals</span></div>
+        <div class="mb-3 flex justify-between"><span class="text-2xl">Level Progression</span> <span class="text-lg">({{ gurudKanji }} / {{ kanjiGurudToLevel }}) Kanji Guru'd to Level</span></div>
+        <div id="level-progress-bar" class="flex flex-row justify-between px-1 mb-3">
+            <div v-for="index in kanjiGurudToLevel" :key="index"  class="border h-6 w-full mx-0.5" :style="{ backgroundColor: (gurudKanji >= index) ? 'limegreen' : 'gray' }"></div>
+        </div>
+        <div class="mb-1"><span class="text-xl">Radicals ({{ gurudRadicals }}/{{ radicals.length }})</span></div>
+        <hr class="mb-3" />
         <div class="mb-6 flex flex-row flex-wrap">
             <div v-for="r in radicals" :key="r.subject.id">
                 <SubjectBadge :subject="r.subject" :assignment="r.assignment" />
             </div>  
         </div>
-        <div class="mb-3"><span class="text-xl">Kanji</span></div>
+        <div class="mb-1"><span class="text-xl">Kanji ({{ gurudKanji }}/{{ kanji.length }})</span></div>
+        <hr class="mb-3" />
         <div class="mb-6 flex flex-row flex-wrap">
             <div v-for="k in kanji" :key="k.subject.id">
                 <SubjectBadge :subject="k.subject" :assignment="k.assignment" />
             </div>  
         </div>
-        <div class="mb-3"><span class="text-xl">Vocabulary</span></div>
+        <div class="mb-1"><button @click="toggleVocabulary" class="text-xl border border-purple-500 bg-purple-500 text-white hover:bg-purple-600">Vocabulary ({{ gurudVocabulary }}/{{ vocabulary.length }})</button></div>
+        <hr class="mb-3" />
         <div class="mb-6 flex flex-row flex-wrap">
-            <div v-for="v in vocabulary" :key="v.subject.id">
+            <div :class="{ 'hidden': !showVocabulary }" v-for="v in vocabulary" :key="v.subject.id">
                 <SubjectBadge :subject="v.subject" :assignment="v.assignment" />
             </div>  
         </div>
     </div>
 </template>
+
+<style scoped>
+    #level-progress-bar > div:first-child {
+        border-top-left-radius: 10px;
+        border-bottom-left-radius: 10px;
+    }
+
+    #level-progress-bar > div:last-child {
+        border-top-right-radius: 10px;
+        border-bottom-right-radius: 10px;
+    }
+</style>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue';
@@ -34,6 +52,7 @@ const assignmentStore = useAssignmentStore();
 const userStore = useUserStore();
 
 let displayedLevel = ref(userStore.user.userDetails?.data.level || 0);
+let showVocabulary = ref(false);
 
 interface SubjectAssignmentPair {
     subject: WaniKani.WaniKaniResource<WaniKani.Subject>,
@@ -64,6 +83,27 @@ const kanji = computed(() => {
 const vocabulary = computed(() => {
     return dataForLevel.value.filter(data => data.subject.object === 'vocabulary' || data.subject.object === 'kana_vocabulary');
 });
+
+const gurudRadicals = computed(() => {
+    return radicals.value.filter(s => s.assignment?.data.passedAt != null).length;
+});
+
+const gurudKanji = computed(() => {
+    return kanji.value.filter(s => s.assignment?.data.passedAt != null).length;
+});
+
+// 90% of the kanji need to be gurud to reach the next level rounded up 
+const kanjiGurudToLevel = computed(() => {
+    return Math.ceil(kanji.value.length * 0.9); 
+});
+
+const gurudVocabulary = computed(() => {
+    return vocabulary.value.filter(s => s.assignment?.data.passedAt != null).length;
+});
+
+function toggleVocabulary() {
+    showVocabulary.value = !showVocabulary.value;
+}
 
 //TODO Create SubjectBadge components and begin building out Level Progression UI
 function sortBySubjectId(ascending: boolean) {
--- src/components/SubjectBadge.vue
+++ src/components/SubjectBadge.vue
@@ -1,24 +1,37 @@
 <template>
-    <div :style="badgeStyle " class="w-fit h-fit border rounded-md m-1 text-">
-        <button>{{ props.subject.data.characters }}</button>
+    <div :style="badgeStyle " class="w-fit h-fit border rounded-md m-1">
+        <button class="badge-button">{{ props.subject.data.characters }}</button>
     </div>
     <div class="flex flex-row justify-between px-1" v-if="props.assignment != null && props.assignment.data.startedAt != null">
-        <div v-for="index in 5" :key="index" :class="`level-progress ${(props.assignment?.data.passedAt != null || props.assignment?.data.srsStage >= index) ? 'passed' : ''}`"></div>
+        <div v-for="index in 5" :key="index" :class="`level-progress border-[.5px] w-full mx-[1px] ${(props.assignment?.data.passedAt != null || props.assignment?.data.srsStage >= index) ? 'passed' : ''}`"></div>
     </div>
 </template>
 
 <style scoped>
+    .badge-button {
+        padding: 0.3rem 0.6rem;
+        font-size: 1.5rem;
+        font-weight: lighter;
+        color: white;
+    }
+
     .level-progress {
-        width: 15%;
         height: 5px;
-        background-color: var(--color-gray-500);
-        border-radius: 10px;
-        border-color: white;
-        border-width: .5px;
+        background-color: gray;
     }
 
     .level-progress.passed {
-        background-color: var(--color-green-500);
+        background-color: limegreen;
+    }
+
+    .level-progress:first-child {
+        border-top-left-radius: 10px;
+        border-bottom-left-radius: 10px;
+    }
+
+    .level-progress:last-child {
+        border-top-right-radius: 10px;
+        border-bottom-right-radius: 10px;
     }
 </style>
 
